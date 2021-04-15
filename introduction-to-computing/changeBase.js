8e3e1210ed5d55b8b48dac8a4b964d93f5b308e9
Gustavo A <atencio361@hotmail.com>
2021-04-15 15:11:36 -0500

    Adding hexadecimal digits

--- introduction-to-computing/changeBase.js
+++ introduction-to-computing/changeBase.js
@@ -3,29 +3,11 @@ const readline = require('readline').createInterface({
   output: process.stdout
 })
 
-/**
- * @param {Number} dividend
- * @param {Number} divisor
- * @returns {String}
- */
-const getRemainder = (dividend, divisor) => {
-  if (isNaN(dividend) || isNaN(divisor)) throw new TypeError('Arguments must be numbers')
-  if (dividend % divisor >= 10) {
-    if (dividend % divisor === 10) return 'A'
-    else if (dividend % divisor === 11) return 'B'
-    else if (dividend % divisor === 12) return 'C'
-    else if (dividend % divisor === 13) return 'D'
-    else if (dividend % divisor === 14) return 'E'
-    else if (dividend % divisor === 15) return 'F'
-    else return `(${dividend % divisor})`
-  }
-  return `${dividend % divisor}`
-}
-
 // Another way to use numbers in Javascript because in this the numbers are implemented are really bad
 
 /**
- * @param {String} strNumber
+ * @param {String|Number} strNumber
+ * @returns {String}
  */
 const getInteger = strNumber => {
   if (isNaN(strNumber)) throw new TypeError('Argument most be a strNumber')
@@ -37,12 +19,44 @@ const getInteger = strNumber => {
  */
 const getDecimals = strNumber => {
   if (isNaN(strNumber)) throw new TypeError('Argument most be a number')
-  let decimals = 0
+  let decimalPart = 0
   let decimalsIndex = strNumber.indexOf('.')
   if (decimalsIndex !== -1) {
-    decimals = parseFloat(`0.${strNumber.slice(decimalsIndex + 1)}`)
+    decimalPart = parseFloat(`0.${strNumber.slice(decimalsIndex + 1)}`)
   }
-  return decimals.toString()
+  return decimalPart.toString()
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {Number} value
+ * @returns the representation of a number greater than 10 in hexadecimal or the value entered if it isn't in the range
+ */
+const hexadecimalDigits = value => {
+  if (isNaN(value)) throw new TypeError('Argument must be a number')
+  const strValue = getInteger(value)
+  if (Number(strValue) <= 15) {
+    if (strValue === '10') return 'A'
+    else if (strValue === '11') return 'B'
+    else if (strValue === '12') return 'C'
+    else if (strValue === '13') return 'D'
+    else if (strValue === '14') return 'E'
+    else if (strValue === '15') return 'F'
+    else return strValue
+  } else {
+    return `(${strValue})`
+  }
+}
+
+/**
+ * @param {Number} dividend
+ * @param {Number} divisor
+ * @returns {String}
+ */
+const getRemainder = (dividend, divisor) => {
+  if (isNaN(dividend) || isNaN(divisor)) throw new TypeError('Arguments must be numbers')
+  return hexadecimalDigits(dividend % divisor)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -55,7 +69,7 @@ const getDecimals = strNumber => {
  * @returns {String}
  */
 const integerBaseTo = (base, integer) => {
-  if (integer < base) return integer
+  if (integer < base) return hexadecimalDigits(integer)
   const quotient = getInteger(integer / base)
   const remainder = getRemainder(integer, base)
   return `${integerBaseTo(base, quotient)}${remainder}`
@@ -63,26 +77,26 @@ const integerBaseTo = (base, integer) => {
 
 /**
  * @param {Number} base
- * @param {Number} decimals
+ * @param {Number} decimalPart
  * @param {Number} precision
  * @returns {String}
  */
-const decimalsBaseTo = (base, decimals, precision = 0) => {
+const decimalsBaseTo = (base, decimalPart, precision = 0) => {
   if (precision === 10) return ''
-  if (base * decimals === 0) return 0
-  const numberXdecimals = decimals * base
-  const digitToLeave = getInteger(numberXdecimals)
-  const remainder = getDecimals(numberXdecimals.toString()) - getDecimals(digitToLeave.toString())
+  if (base * decimalPart === 0) return 0
+  const numberXdecimals = decimalPart * base
+  const digitToLeave = hexadecimalDigits(getInteger(numberXdecimals))
+  const remainder = getDecimals(numberXdecimals.toString()) - getDecimals(digitToLeave)
   return `${digitToLeave}${decimalsBaseTo(base, remainder, ++precision)}`
 }
 
-const floatBaseTo = (base, strNumber) => {
-  let integerPart = getInteger(strNumber)
-  let decimals = getDecimals(strNumber)
+const floatBaseDecimalTo = (base, strNumber) => {
+  const integerPart = integerBaseTo(base, getInteger(strNumber))
+  let decimalPart = decimalsBaseTo(base, getDecimals(strNumber))
   return {
-    number: `${integerBaseTo(base, integerPart)}.${decimalsBaseTo(base, decimals)}`,
-    integer: integerBaseTo(base, integerPart),
-    decimal: `0.${decimalsBaseTo(base, decimals)}`
+    number: `${integerPart}${decimalPart === 0 ? '' : `.${decimalPart}`}`,
+    integer: integerPart,
+    decimal: `0.${decimalPart}`
   }
 }
 
@@ -99,10 +113,10 @@ readline.on('line', line => {
     return
   }
 
-  const newNumber = floatBaseTo(2, line)
+  const newNumber = floatBaseDecimalTo(2, line)
   console.log(newNumber.integer)
   console.log(newNumber.decimal)
-  console.log(Number(newNumber.number))
+  console.log(newNumber.number)
 
   console.log()
   console.log('Write another number:')
