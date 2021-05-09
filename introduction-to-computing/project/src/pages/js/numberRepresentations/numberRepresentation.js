3c6bec6652795c3e7f3d305db54c3888fd0c12fb
alexanderdiegocv <alexanderdiego2001@outlook.com>
2021-05-09 00:24:40 -0500

    Actualizando con funciones

--- introduction-to-computing/project/src/pages/js/numberRepresentations/numberRepresentation.js
+++ introduction-to-computing/project/src/pages/js/numberRepresentations/numberRepresentation.js
@@ -12,7 +12,7 @@ import firstDigit from '../changeBase/firstDigit.js'
 console.log(encodeToExcess(127, 10))
 console.log(encodeToExcess(1023, 10))
 
-const Disestructure = (bias, number) => {
+const Deconstruct = (bias, number) => {
   const digit = firstDigit(number.toString())
   const sign = digit.asNumber == 1 ? '-' : ''
   const restonumber = number.slice(1)
@@ -27,38 +27,44 @@ const Disestructure = (bias, number) => {
   }
   return {
     sign: sign,
-    // number: number,
-    // restofnumber: restonumber,
     shift: shift,
     mantissa: mantissa
   }
 }
 
-const ExcessToDecimal = (bias, number) => {
+const BuildNumber = (sign, mantissa, exp) => {
+  let completeform = `1.${mantissa}`
+  if(completeform.indexOf(".") !== -1){
+    completeform = completeform.replace('.', '')
+  }
+  let part1 = completeform.slice(0, exp + 1)
+  let part2 = completeform.slice(exp + 1)
+
+  completeform = part1.concat('.', part2)
+
+  let Decimalnumber = anyToDecimal(2, completeform).number
+  Decimalnumber = sign.concat('', Decimalnumber)
+
+  return Decimalnumber
+}
+
+export const ExcessToDecimal = (bias, number) => {
   if (typeof bias !== 'number') throw new TypeError('Bias must be a number')
   if (bias !== 127 && bias !== 1023) throw new Error('Invalid, bias must be 127 or 1023')
-  let exponentLength
-  let mantissaLength
+
   let exponent
+
   if (bias === 127) {
-    exponent = anyToDecimal(2, Disestructure(bias, number).shift).integer // string
+    exponent = anyToDecimal(2, Deconstruct(bias, number).shift).integer
     exponent = exponent - bias
   } else {
-    exponent = anyToDecimal(2, Disestructure(bias, number).shift).integer // string
+    exponent = anyToDecimal(2, Deconstruct(bias, number).shift).integer
     exponent = exponent - bias
   }
 
-  const sign = Disestructure(bias, number).sign // + o - en string
-  const mantissa = Disestructure(bias, number).mantissa //string
-
-  let res = `1.${mantissa}`
-  res = res.replace('.', '')
-  let res1 = res.slice(0, exponent + 1)
-  let res2 = res.slice(exponent + 1)
-  res = res1.concat('.', res2)
-
-  let numeroDecimal = anyToDecimal(2, res).number
-  numeroDecimal = sign.concat('', numeroDecimal)
+  const sign = Deconstruct(bias, number).sign
+  const mantissa = Deconstruct(bias, number).mantissa
+  const numeroDecimal = BuildNumber(sign, mantissa, exponent)
 
   return {
     signo: sign,
@@ -68,11 +74,5 @@ const ExcessToDecimal = (bias, number) => {
   }
 }
 
-// console.log(excess(1023, 0.0025))
-// console.log(excess(1023, -0.0025))
-// console.log(excess(1023, 0.0625))
-// console.log(excess(1023, -0.0625))
-// console.log(excess(1023, 0.625))
-// console.log(excess(1023, -1.225))
-// console.log(excess(1023, 25))
 console.log(ExcessToDecimal(127, '11001010000000000111000100001111'))
+console.log(ExcessToDecimal(127, '01001010000000000111000100001111'))
