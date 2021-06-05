90105f921a0d548ffd0706fa964da45d34e90ae5
Gustavo A <atencio361@hotmail.com>
2021-06-05 18:23:08 -0500

    Force complement

--- introduction-to-computing/project/src/pages/js/binaryOperations/complement/encodeComplement.js
+++ introduction-to-computing/project/src/pages/js/binaryOperations/complement/encodeComplement.js
@@ -5,14 +5,13 @@ import completeWithZeros from '../../numberRepresentations/completeWithZeros.js'
 import findNearestLenght from '../../numberRepresentations/findNearestLenght.js'
 import addBits from '../aritmetic/integers/addBits.js'
 
-export const complementToOne = ({ number: strNumber, base }) => {
+export const complementToOne = ({ number: strNumber, base, force = false }) => {
   if (typeof strNumber !== 'string') throw new TypeError('Number must be a number type string')
-  if (typeof base !== 'number') throw new TypeError('Base must be a number')
   if (!isHex(strNumber)) throw new Error('Number does not exist')
 
   let isPositive = strNumber.slice(0, 1) !== '-'
-  let number = strNumber
-  let recomendedBits = findNearestLenght(strNumber.length + 1, 2)
+  let number = strNumber.replace('-', '')
+  let recomendedBits = findNearestLenght(strNumber.length, 2)
 
   if (base !== 2) {
     const decimalNumber = anyToDecimal(base, number)
@@ -21,28 +20,24 @@ export const complementToOne = ({ number: strNumber, base }) => {
     number = binaryNumber.unsignedNumber
   }
 
-  let completeNumber = completeWithZeros({ unsignedNumber: number }, recomendedBits)
+  const completeNumber = completeWithZeros({ unsignedNumber: number }, recomendedBits)
+  const invertedBits = [...completeNumber].map(bit => (bit === '1' ? '0' : '1')).join('')
 
-  if (isPositive) {
-    return { type: 'c1', number: completeNumber, sign: '0' }
-  }
+  if (force) return { type: 'c1', number: invertedBits }
 
-  const invertedrecomendedBits = [...completeNumber]
-    .map(bit => (bit === '1' ? '0' : '1'))
-    .join('')
-    .slice(1)
+  if (isPositive) return { type: 'c1', number: completeNumber, sign: '0' }
 
-  return { type: 'c1', number: `1${invertedrecomendedBits}`, sign: '1' }
+  return { type: 'c1', number: `1${invertedBits.slice(1)}`, sign: '1' }
 }
 
-export const complementToTwo = ({ number: strNumber, base, bits = 0 }) => {
+export const complementToTwo = ({ number: strNumber, base, bits = 0, force = false }) => {
   if (typeof strNumber !== 'string') throw new TypeError('Number must be a number type string')
   if (typeof base !== 'number') throw new TypeError('Base must be a number')
   if (!isHex(strNumber)) throw new Error('Number does not exist')
 
-  const numberInC1 = complementToOne({ number: strNumber, base, bits })
+  const numberInC1 = complementToOne({ number: strNumber, base, bits, force })
   if (numberInC1.sign === '0') {
-    return { type: 'c2', number: numberInC1.number }
+    return { type: 'c2', number: numberInC1.number, sign: '0' }
   }
 
   return { type: 'c2', number: addBits(numberInC1.number, '1') }
