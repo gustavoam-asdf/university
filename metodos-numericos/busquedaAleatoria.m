1e61bee41708bb12e7747f67076203659c1f63ff
Gustavo A <atencio361@hotmail.com>
2021-07-05 11:35:17 -0500

    Binary search method

---
+++ metodos-numericos/busquedaAleatoria.m
@@ -0,0 +1,40 @@
+clear;
+close all;
+clc;
+
+syms x y
+fxy = y - x - 2*x^2- 2*x*y - y^2;
+dMin = -2;
+dMax = 2;
+rMin = 1;
+rMax = 3;
+
+ezplot(fxy, [dMin dMax rMin rMax]);
+
+
+maxF = -1e-9;
+
+maxIter = 1e2;
+
+fprintf('It.\t\tx\t\t\t\ty\t\t\tf(x,y)\n');
+for j = 1:maxIter 
+
+  nX = dMin + (dMax - dMin) * rand();
+  nY = rMin + (rMax - rMin) * rand();
+  fn = subs(fxy, x, nX);
+  fn = subs(fn, y, nY);
+
+  if fn > maxF
+   maxF = fn;
+   maxX = nX;
+   maxY = nY;
+  end
+  
+  fprintf('%2d\t%7.4f\t%7.4f\t%7.4f\n', j, nX, nY, fn);
+  
+end
+
+fprintf('Máximo x: %7.4f, Máximo y: %7.4f, Fmax: %7.4f\n', maxX, maxY, maxF);
+
+
+
--- metodos-numericos/sem11.m
+++
@@ -1,8 +0,0 @@
-% Deflación polinomial
-
-r = a(n);
-a(n) = 0;
-
-for i = n-1:-1:0
-  
-end
