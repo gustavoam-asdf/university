4f95762edfad9f0e6ef79c0e69bcac80377425b7
Gustavo A <atencio361@hotmail.com>
2021-09-11 20:06:44 -0500

    Determinant of a matrix with recursion

---
+++ algorithm-data-structure/Recursion/src/Matrix/Matrix.java
@@ -0,0 +1,80 @@
+package Matrix;
+
+public class Matrix {
+
+  public static void main(String[] args) {
+    double[][] matrix1 = {
+      {3, 6, -2},
+      {-4, 1, -3},
+      {5, -5, 2}
+    };
+
+    double[][] matrix2 = {
+        {2, -3, -4},
+        {0, 5, 2},
+        {-1, -2, 3}
+    };
+
+    System.out.printf("""
+      Determinante de la primera matriz: %.2f
+      Determinante de la segunda matriz: %.2f
+    """, determinantMatrix(matrix1), determinantMatrix(matrix2));
+
+  }
+
+  public static int signMap (int coorX, int coorY) {
+    if ((coorX + coorY) % 2 == 0)
+      return 1;
+    else
+      return -1;
+  }
+
+  public static double[][] getCofactor (double[][] matrix, int coorX, int coorY) {
+    int length = matrix.length;
+    double[][] cofactor = new double[length - 1][length - 1];
+
+    int cofactorCoorX = 0, cofactorCoorY = 0;
+
+    for (int i = 0; i < length; i++) {
+      for (int j = 0; j < length; j++) {
+
+        if (i != coorX && j != coorY) {
+          cofactor[cofactorCoorX][cofactorCoorY++] = matrix[i][j];
+          if (cofactorCoorY == cofactor.length) {
+            cofactorCoorX++;
+            cofactorCoorY = 0;
+          }
+        }
+        
+      }
+    }
+
+    return cofactor;
+  }
+
+  public static double determinantMatrix (double[][] matrix ) {
+    int length = matrix.length;
+    double determinat = 0;
+
+    if (length == 1) {
+      return  matrix[0][0];
+    }
+
+    if (length == 2) {
+      double mainDiag = matrix[0][0] * matrix[1][1];
+      double secondaryDiag = matrix[0][1] * matrix[1][0];
+      return mainDiag - secondaryDiag;
+    }
+
+    double[] firstLine = matrix[0];
+    int coorX = 0;
+
+    for (int coorY = 0; coorY < length; coorY++) {
+      double[][] cofactor = getCofactor(matrix, coorX, coorY);
+      determinat += signMap(coorX, coorY) * firstLine[coorY] * determinantMatrix(cofactor);
+    }
+
+    return determinat;
+  }
+
+}
--- algorithm-data-structure/Recursion/src/Palindrome/Palindrome.java
+++ algorithm-data-structure/Recursion/src/Palindrome/Palindrome.java
@@ -5,9 +5,7 @@ public class Palindrome {
   public static void main(String[] args) {
     String word = "DABALEARROZALAZORRAELABAD";
     String result = isPalindrome(word) ? "es" : "no es" ;
-
     System.out.printf("La palabra %s %s un palíndromo", word, result);
-
   }
 
   public static boolean isPalindrome (String word) {
@@ -23,9 +21,9 @@ public class Palindrome {
 
     String nextWord = word.substring(1, word.length() - 1);
     System.out.printf("""
-        word:       %s
-        nextWord:    %s
-        """, word, nextWord);
+      word:       %s
+      nextWord:    %s
+    """, word, nextWord);
     System.out.println();
     return isPalindrome(nextWord);
   }
