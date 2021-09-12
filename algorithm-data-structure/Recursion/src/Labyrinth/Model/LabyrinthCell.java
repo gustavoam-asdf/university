52da5f5f2e132c8354c4fd544c1015b7a6080ab3
Gustavo A <atencio361@hotmail.com>
2021-09-12 10:34:55 -0500

    New cell types

---
+++ algorithm-data-structure/Recursion/src/Labyrinth/Model/LabyrinthCell.java
@@ -0,0 +1,41 @@
+package Labyrinth.Model;
+
+public class LabyrinthCell {
+  public final static int TYPE_WAY = 1;
+  public final static int TYPE_WALL = 2;
+  public final static int TYPE_PLAYER = 3;
+  public final static int TYPE_START = 4;
+  public final static int TYPE_END = 5;
+  private int type;
+  private char symbol;
+
+  public LabyrinthCell(int type) {
+    this.setType(type);
+  }
+
+  public int getType () {
+    return this.type;
+  }
+
+  public void setType(int type) {
+    if (type != TYPE_WAY && type != TYPE_WALL && type != TYPE_PLAYER && type != TYPE_START && type != TYPE_END)
+      throw new Error("Unrecognized type");
+    this.type = type;
+    if (type == TYPE_WAY) {
+      this.symbol = '\u3000';
+    } else if (type == TYPE_START) {
+      this.symbol = 'ﱿ';
+    } else if (type == TYPE_WALL) {
+      this.symbol = '▮';
+    } else if (type == TYPE_PLAYER) {
+      this.symbol = '\uE213';
+    } else {
+      this.symbol = '\uF426';
+    }
+  }
+
+  public char getSymbol() {
+    return this.symbol;
+  }
+
+}
