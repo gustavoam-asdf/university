74d0083a01bc2d1049c5fee91fa36bbf7988ea25
Gustavo A <gustavo.atencio@outlook.com>
2021-12-03 17:33:56 +0000

    Reading directories

---
+++ operating-systems/file-system/read-directory/main.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+
+/* Función para devolver un error en caso de que ocurra */
+void error(const char *s);
+/* Función que hace algo con un archivo */
+void procesoArchivo(char* path, char *archivo);
+
+int main(int argc, char *argv[]) {
+  /* Con un puntero a DIR abriremos el directorio */
+  DIR *dir;
+
+	char* path = argv[1];
+
+	if (path == NULL) error("Debe ingresar una ubicacion correcta");
+ 
+  /* en *ent habrá información sobre el archivo que se está "sacando" a cada momento */
+  struct dirent *ent;
+
+  /* Empezaremos a leer en el directorio actual */
+  dir = opendir (path);
+
+  /* Miramos que no haya error */
+  if (dir == NULL)
+    error("No puedo abrir el directorio");
+ 
+  /* Una vez nos aseguramos de que no hay error, ¡vamos a jugar! */
+  /* Leyendo uno a uno todos los archivos que hay */
+  while ((ent = readdir (dir)) != NULL) {
+    /* Nos devolverá el directorio actual (.) y el anterior (..), como hace ls */
+    if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ) {
+  	 	/* Una vez tenemos el archivo, lo pasamos a una función para procesarlo. */
+     	procesoArchivo(path, ent->d_name);
+   	}
+  }
+  closedir (dir);
+
+  return EXIT_SUCCESS;
+}
+
+void error(const char *s) {
+  /* perror() devuelve la cadena S y el error (en cadena de caracteres) que tenga errno */
+  perror (s);
+  exit(EXIT_FAILURE);
+}
+
+void procesoArchivo(char* path, char* archivo) {
+  /* Para "procesar", o al menos, hacer algo con el archivo, vamos a decir su tamaño en bytes */
+  /* para ello haremos lo que vemos aquí: https://poesiabinaria.net/2010/04/tamano-de-un-fichero-en-c/ */
+  FILE* fich;
+  long ftam;
+
+	if (!(path[strlen(path) - 1] == '/')) 
+			path = strcat(path, "/");
+		
+	char* filePath = strcat(path, archivo);
+	
+	printf("Ubicación archivo: %s\n\n\n", filePath);
+
+  fich = fopen(filePath, "r");
+  if (fich) {
+      fseek(fich, 0L, SEEK_END);
+      ftam=ftell(fich);
+      fclose(fich);
+      /* Si todo va bien, decimos el tamaño */
+      //printf ("%30s (%ld bytes)\n", archivo, ftam);
+    }
+//  else
+    /* Si ha pasado algo, sólo decimos el nombre */
+    //printf ("%30s (No info.)\n", archivo);
+}
