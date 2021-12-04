ecd0dbd2e143469ac2310a3cb41450e21f3864e6
Gustavo A <atencio361@hotmail.com>
2021-12-03 19:18:57 -0500

    Using stats to view file attributes

--- operating-systems/file-system/read-directory/main.c
+++ operating-systems/file-system/read-directory/main.c
@@ -1,13 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
 #include <dirent.h>
+
 #include <string.h>
 
 /* Función para devolver un error en caso de que ocurra */
 void error(const char *s);
+
+void printFileProperties(struct stat stats);
+
 /* Función que hace algo con un archivo */
-void procesoArchivo(char *path, char *archivo);
+void fileProcess(char *path, char *archivo);
 
 int main(int argc, char *argv[])
 {
@@ -34,13 +40,14 @@ int main(int argc, char *argv[])
 
   /* Una vez nos aseguramos de que no hay error, ¡vamos a jugar! */
   /* Leyendo uno a uno todos los archivos que hay */
+  printf("Folder: %s\n\n", path);
   while ((ent = readdir(dir)) != NULL)
   {
     /* Nos devolverá el directorio actual (.) y el anterior (..), como hace ls */
     if ((strcmp(ent->d_name, ".") != 0) && (strcmp(ent->d_name, "..") != 0))
     {
       /* Una vez tenemos el archivo, lo pasamos a una función para procesarlo. */
-      procesoArchivo(path, ent->d_name);
+      fileProcess(path, ent->d_name);
     }
   }
   closedir(dir);
@@ -55,26 +62,47 @@ void error(const char *s)
   exit(EXIT_FAILURE);
 }
 
-void procesoArchivo(char *path, char *fileName)
+void fileProcess(char *path, char *fileName)
 {
-  /* Para "procesar", o al menos, hacer algo con el archivo, vamos a decir su tamaño en bytes */
-  /* para ello haremos lo que vemos aquí: https://poesiabinaria.net/2010/04/tamano-de-un-fichero-en-c/ */
-  FILE *file;
-  long fileLength;
+  struct stat stats;
 
   char filePath[strlen(path) + strlen(fileName)];
   strcpy(filePath, path);
   strcat(filePath, fileName);
 
-  file = fopen(filePath, "r");
-  if (!file)
-    /* Si ha pasado algo, sólo decimos el nombre */
-    printf("%50s (No info.)\n", fileName);
+  if (stat(filePath, &stats) == -1)
+    printf("Unable to get file properties for %s\n", filePath);
 
-  // Leemos el archivo de inicio a fin
-  fseek(file, SEEK_SET, SEEK_END);
-  fileLength = ftell(file);
-  fclose(file);
-  /* Si todo va bien, decimos el tamaño */
-  printf("%50s (%ld bytes)\n", fileName, fileLength);
+  printf("\nFile: %s", fileName);
+  printFileProperties(stats);
+}
+
+void printFileProperties(struct stat stats)
+{
+  struct tm dt;
+
+  // File permissions
+  printf("\nFile access: ");
+  if (stats.st_mode & R_OK)
+    printf("read ");
+  if (stats.st_mode & W_OK)
+    printf("write ");
+  if (stats.st_mode & X_OK)
+    printf("execute");
+
+  // File size
+  printf("\nFile size: %ld", stats.st_size);
+
+  // Get file creation time in seconds and
+  // convert seconds to date and time format
+  dt = *(gmtime(&stats.st_ctime));
+  printf("\nCreated on: %d-%d-%d %d:%d:%d", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900,
+         dt.tm_hour, dt.tm_min, dt.tm_sec);
+
+  // File modification time
+  dt = *(gmtime(&stats.st_mtime));
+  printf("\nModified on: %d-%d-%d %d:%d:%d", dt.tm_mday, dt.tm_mon, dt.tm_year + 1900,
+         dt.tm_hour, dt.tm_min, dt.tm_sec);
+
+  printf("\n\n");
 }
