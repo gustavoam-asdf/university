ad53933641c09568f7c95d3d488e72e7699c9aa1
Gustavo A <gustavo.atencio@outlook.com>
2022-05-11 21:13:04 -0500

    script to create database backup

---
+++ database-management/engines/SQLServer/createBackup.ps1
@@ -0,0 +1,7 @@
+$backupDir = "$github\university\database-management\database\logistica_unheval\sql-server\backup"
+$database = "logistica_unheval"
+$date = Get-Date -Format yyyy_MM_dd_HH_mm_ss
+
+sqlcmd -Q "BACKUP DATABASE $database TO DISK='$("$backupDir\$($database)_$($date).bak")'" -S GUS\SQLEXPRESS
+
+Write-Host "Backup was created successfully in $backupDir"
