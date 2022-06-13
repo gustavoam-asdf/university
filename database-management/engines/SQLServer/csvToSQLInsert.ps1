d37790ec0827d9bffa816ca02eadd1f27072d2f9
Gustavo A <gustavo.atencio@outlook.com>
2022-06-13 12:41:17 -0500

    using functions and saving tables types

--- database-management/engines/SQLServer/csvToSQLInsert.ps1
+++ database-management/engines/SQLServer/csvToSQLInsert.ps1
@@ -1,20 +1,110 @@
+$VentasCotizacion = "Ventas.cotizacion"
+$OrganizacionSolicitud = "Organizacion.solicitud"
+$OrganizacionSolicitud_detalle = "Organizacion.solicitud_detalle"
+$VentasOrden_compra = "Ventas.orden_compra"
+$VentasOrden_compra_detalle = "Ventas.orden_compra_detalle"
+$VentasFactura = "Ventas.factura"
+$VentasFactura_detalle = "Ventas.factura_detalle"
+$InventariosSalida = "Inventarios.salida"
+$InventariosSalida_detalle = "Inventarios.salida_detalle"
 
-$table = "RecursosHumanos.contrato"
-$csvPath = "C:/Users/GUSTAVO/Downloads/$table.csv"
+function Get-RowTypes ($table) {
+	if ($table -eq $VentasCotizacion) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"number",
+			"number"
+		)
+	}
 
-$rows = $(Get-Content $csvPath).Split('\n')
-$dataLength = $rows.Length - 1
+	if ($table -eq $OrganizacionSolicitud) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"number",
+			"number",
+			"date",
+			"number"
+		)
+	}
 
-$headers = $rows[0]
-$rowTypes = @(
-	"number",
-	"number",
-	"number",
-	"number",
-	"date",
-	"date"
-)
-$content = $rows[1..$dataLength]
+	if ($table -eq $OrganizacionSolicitud_detalle) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"number"
+		)
+	}
+
+	if ($table -eq $VentasOrden_compra) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"date",
+			"date",
+			"number",
+			"number"
+		)
+	}
+
+	if ($table -eq $VentasOrden_compra_detalle) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"number",
+			"number",
+			"number"
+		)
+	}
+	
+	if ($table -eq $VentasFactura) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"date",
+			"string",
+			"number",
+			"number"
+		)
+	}
+
+	if ($table -eq $VentasFactura_detalle) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"number"
+		)
+	}
+
+	if ($table -eq $InventariosSalida) {
+		return @(
+			"number",
+			"number",
+			"date",
+			"number"
+		)
+	}
+
+	if ($table -eq $InventariosSalida_detalle) {
+		return @(
+			"number",
+			"number",
+			"number",
+			"number",
+			"number",
+			"string"
+		)
+	}
+	
+}
 
 function typeFormatter {
 	param (
@@ -35,24 +125,48 @@ function typeFormatter {
 
 }
 
-$values = ""
-foreach ($dataRow in $content) {
-	$crudeRow = $dataRow.Split(',')
-	$row = ""
-	for ($i = 0; $i -lt $rowTypes.Length; $i++) {
-		$rowType = $rowTypes[$i]
-		$crudeValue = $crudeRow[$i]
-		$formattedValue = typeFormatter $rowType $crudeValue
-		$row = $row + $formattedValue + ","
+function	makeQuery {
+	param (
+		[string] $table,
+		[string] $headers,
+		[string[]] $rowTypes,
+		[string[]] $content
+	)
+	$values = ""
+	foreach ($dataRow in $content) {
+		$crudeRow = $dataRow.Split(',')
+		$row = ""
+		for ($i = 0; $i -lt $rowTypes.Length; $i++) {
+			$rowType = $rowTypes[$i]
+			$crudeValue = $crudeRow[$i]
+			$formattedValue = typeFormatter $rowType $crudeValue
+			$row = $row + $formattedValue + ","
+		}
+		$row = $row.TrimEnd(',')
+		$values = $values + "	(" + $row + "), " + "`n"
 	}
-	$row = $row.TrimEnd(',')
-	$values = $values + "	(" + $row + "), " + "`n"
+	$values = $values.Trim().TrimEnd(', ')
+	$query = "SET identity_insert $table ON; `n
+	INSERT INTO $table 
+		($headers) 
+	VALUES 
+		$values;
+	SET identity_insert $table OFF;"
+	return $query
 }
-$values = $values.Trim().TrimEnd(', ')
 
-$query = "
-INSERT INTO $table 
-	($headers) 
-VALUES 
-	$values"
+$table = $InventariosSalida_detalle
+$csvPath = "C:/Users/GUSTAVO/Downloads/$table.csv"
+
+$rows = $(Get-Content $csvPath).Split('\n')
+$dataLength = $rows.Length - 1
+$headers = $rows[0]
+$content = $rows[1..$dataLength]
+
+$rowTypes = Get-RowTypes $table
+
+
+
+$query = makeQuery -table $table -headers $headers -rowTypes $rowTypes -content $content
+
 Write-Output $query | clip
