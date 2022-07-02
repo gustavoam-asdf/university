f7b2e5f26c742bc6cd7b64f5f0f356461480547d
Gustavo A <gustavo.atencio@outlook.com>
2022-07-01 23:57:26 -0500

    run

--- database-management/database/run.sql
+++ database-management/database/run.sql
@@ -1,13 +1,10 @@
-SELECT
-  a.nombre area,
-  i.nombre item,
-  sd.cantidad_requerida,
-  i.unidad_medida
-FROM
-  solicitud_detalle sd
-  INNER JOIN solicitud s ON s.nro_solicitud = sd.nro_solicitud
-  INNER JOIN item i ON i.id_item = sd.id_item
-  INNER JOIN area a ON a.id_area = s.id_area
-ORDER BY
-  a.nombre,
-  i.nombre
+execute Ventas.pedidos_por_trabajador '20190101', '20220101'
+execute Ventas.montos_por_proveedor
+execute Inventarios.kardex_producto 1
+
+select id_cotizacion, id_item, valor_unitario, id_proveedor, cantidad_disponible from Ventas.cotizacion
+where valor_unitario = (select MIN(valor_unitario) from Ventas.cotizacion
+where id_item = 1)
+
+select * from Inventarios.get_kardex(1)
+for json path
